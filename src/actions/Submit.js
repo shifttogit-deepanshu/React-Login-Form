b5032fc7490627c6e45efb2b661135ec8f488620
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-05 13:13:02 +0530

    working state...

--- src/actions/Submit.js
+++ src/actions/Submit.js
@@ -5,6 +5,7 @@ export default (email,password)=>{
             snapshot.forEach((childSnapshot)=>{
                 if(childSnapshot.val().email==email && childSnapshot.val().password==password){
                     dispatch({type:"AUTH",id:childSnapshot.key})
+                    
                 }
                 else{
                     console.log("no items")
