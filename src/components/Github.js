46a82c2ee86039c7cdbc2b33cdb256bc77cbe021
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-09 02:41:48 +0530

    added styles to third party component app

--- src/components/Github.js
+++ src/components/Github.js
@@ -2,11 +2,12 @@
 import Auth from "../actions/Auth";
 import store from "../redux/store";
 import {githubAuthProvider} from "../firebase/firebase"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "../icons/fontAwesome/fontAwesome"
 
 export const Github = ()=>{
 return (
-    <div><button onClick={store.dispatch(Auth(githubAuthProvider))}>Sign In with Github</button></div>
+    <div><button onClick={store.dispatch(Auth(githubAuthProvider))} className="btn github"><span className="icon"><FontAwesomeIcon icon={['fab', 'github']} /></span>Sign In with Github</button></div>
 )
 }
 
--- src/components/Google.js
+++ src/components/Google.js
@@ -2,11 +2,13 @@
 import Auth from "../actions/Auth";
 import store from "../redux/store";
 import {googleAuthProvider} from "../firebase/firebase"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "../icons/fontAwesome/fontAwesome"
 
 
 export const Google = ()=>{
 return (
-    <div><button onClick={store.dispatch(Auth(googleAuthProvider))}>Sign In with Google</button></div>
+    <div><button onClick={store.dispatch(Auth(googleAuthProvider))} className="btn google"><span className="icon"><FontAwesomeIcon icon={['fab', 'google']} /></span>Sign In with Google</button></div>
 )
 }
 
