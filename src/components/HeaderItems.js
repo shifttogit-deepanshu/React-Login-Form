ebfc1c711667d1537fe50aa14ff30343c636dfd9
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-10 06:29:50 +0530

    created mobile header

--- src/components/HeaderItems.js
+++ src/components/HeaderItems.js
@@ -2,7 +2,7 @@ import React from "react"
 import {NavLink} from 'react-router-dom'
 
 const HeaderItems = ()=>(
-        <div className="container">
+        <div className="container container--header">
                 <NavLink to="/" className="link_text container--link">Home</NavLink>
                 <NavLink to="/public" className="link_text container--link">Public</NavLink>
                 <NavLink to="/private" className="link_text container--link">Private</NavLink>
