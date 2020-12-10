3a22aedb912fd0a5e1a357ec733f07b63cfb47b6
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-10 17:25:15 +0530

    added Header toggle logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React,{useState} from "react"
 import LoginError from "../components/LoginError"
 import HeaderItems from "./HeaderItems"
-import "../icons/fontAwesome/fontAwesome"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Toggler from "./Toggler"
 
+const Header = ()=>{
+    const breakpoint = window.innerWidth>=769
+    const [toggleStatus,changeStatus] = useState(breakpoint)
 
-const Header = ()=>(
+    return (
     <div className="header">
-        <div className="header__toggler"><FontAwesomeIcon icon={['fas','plus']}/></div>
-        <HeaderItems/>
+        {!breakpoint && <div onClick={()=>changeStatus(!toggleStatus)}><Toggler toggleStatus={toggleStatus} />{toggleStatus}</div>}
+        {toggleStatus && <HeaderItems/>}
     <div>
         <LoginError />
     </div>
     </div>
-)
-
+    )
+}
 export default Header
---
+++ src/components/Toggler.js
@@ -0,0 +1,8 @@
+import React from "react"
+import "../icons/fontAwesome/fontAwesome"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Toggler = ()=>{
+    return (<div className="toggler"><FontAwesomeIcon icon={['fas','plus']}/></div>)
+}
+export default Toggler
