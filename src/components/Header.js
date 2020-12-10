cef6cbf25178ee79a5cd68c7fddf6a6fb5580cbc
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-10 07:31:07 +0530

    added toggle button'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from "react"
 import LoginError from "../components/LoginError"
 import HeaderItems from "./HeaderItems"
+import "../icons/fontAwesome/fontAwesome"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const Header = ()=>(
     <div className="header">
+        <div className="header__toggler"><FontAwesomeIcon icon={['fas','plus']}/></div>
         <HeaderItems/>
     <div>
         <LoginError />
