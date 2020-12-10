0c7015a14e493bcb1184db72e0babaa0daf5c235
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-12-11 01:28:36 +0530

    Header made fully responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,10 @@ import Toggler from "./Toggler"
 const Header = ()=>{
     const breakpoint = window.innerWidth>=769
     const [toggleStatus,changeStatus] = useState(breakpoint)
-
+    const wrapperClassName = toggleStatus ? "wrapper-active" : "wrapper"
     return (
     <div className="header">
-        {!breakpoint && <div onClick={()=>changeStatus(!toggleStatus)}><Toggler toggleStatus={toggleStatus} />{toggleStatus}</div>}
+        {!breakpoint && <div onClick={()=>changeStatus(!toggleStatus)} className={wrapperClassName}><Toggler toggleStatus={toggleStatus} /></div>}
         {toggleStatus && <HeaderItems/>}
     <div>
         <LoginError />
--- src/components/Toggler.js
+++ src/components/Toggler.js
@@ -3,7 +3,7 @@ import "../icons/fontAwesome/fontAwesome"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Toggler = (props)=>{
-    const className = props.toggleStatus ? "toggler toggler-active" : "toggler toggler-unactive"
+    const className = props.toggleStatus ? "toggler toggler-active" : "toggler"
     return (<div className={className} ><FontAwesomeIcon icon={['fas','plus']}/></div>)
 }
 export default Toggler
